bbe5bad1d91a1bbf246f22d3783b04d05e599cb7
Jade Kazoo <jadekzn@gmail.com>
2017-11-30 09:28:46 -0500

    Added users and teams and some starting data in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.find_or_create_by(name: 'Pigsnout')
+Team.find_or_create_by(name: 'Gay Tonks Club')
+Team.find_or_create_by(name: 'Burn Umbridges')
+Team.find_or_create_by(name: 'Slytherin')
+
+3.times do
+	User.create(
+		name: Faker::Name.name_with_middle,
+		team: Team.all.sample
+		)
+end
