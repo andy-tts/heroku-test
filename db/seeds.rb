8faeeac822d19c39fe7826738d540d27d7b6eccc
Andy TTS <awhillans@techtalentsouth.com>
2017-11-30 09:28:31 -0500

    Generate scaffold for users, make team model, seed both in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.find_or_create_by(name: 'Hufflepuff')
+Team.find_or_create_by(name: 'Slytherin')
+Team.find_or_create_by(name: 'Griffindor')
+Team.find_or_create_by(name: 'Ravenclaw')
+
+3.times do 
+	User.create(
+		name: Faker::Simpsons.character,
+		team: Team.all.sample
+	)
+end
